3dbdbc19f289334e99873b3784d6faac5df175e8
Fred <wuxd1302@gmail.com>
2024-01-09 15:02:23 -0800

    eureka server

---
+++ src/test/java/mf/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package mf.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
