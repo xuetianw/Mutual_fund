37f951f7ef33e67aef863ad2fe52e584d6f4c23d
Fred <wuxd1302@gmail.com>
2024-01-10 14:42:32 -0800

    renaming

--- src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.example.apigateway1259;
+package com.example.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
