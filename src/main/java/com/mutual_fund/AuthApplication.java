d88852fcf6e1adb46537cd519e0054c59e225d96
Fred <wuxd1302@gmail.com>
2024-01-11 15:18:49 -0800

    rename file name

--- src/main/java/com/mutual_fund/AuthApplication.java
+++ src/main/java/com/mutual_fund/AuthApplication.java
@@ -6,15 +6,15 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JwtDemoApplication extends SpringBootServletInitializer {
+public class AuthApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JwtDemoApplication.class, args);
+		SpringApplication.run(AuthApplication.class, args);
 	}
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
-		return builder.sources(JwtDemoApplication.class);
+		return builder.sources(AuthApplication.class);
 	}
 
 }
