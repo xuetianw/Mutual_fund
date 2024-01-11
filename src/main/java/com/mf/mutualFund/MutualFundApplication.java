5577184cb72bbc9b1f00b1837232daa8f64b3f71
Fred <wuxd1302@gmail.com>
2024-01-11 15:58:57 -0800

    mf service

--- src/main/java/com/mf/mutualFund/MutualFundApplication.java
+++ src/main/java/com/mf/mutualFund/MutualFundApplication.java
@@ -2,6 +2,7 @@ package com.mf.mutualFund;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
@@ -9,6 +10,12 @@ public class MutualFundApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(MutualFundApplication.class, args);
+	}
+
+
+	@Override
+	public SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+		return builder.sources(SpringApplication.class);
 	}
 
 }
