eb24e1f21aa14a8d4abbb8536649d172795d9f1e
Fred <wuxd1302@gmail.com>
2024-02-01 16:47:06 -0800

    change all rest apis to calling the gateway

--- src/url.js
+++ src/url.js
@@ -37,42 +37,90 @@
 
 
 // Note: below is test in local:
-const url = "http://localhost";
+// const url = "http://localhost";
+// const url = "http://35.183.144.38";
 
-const authPort = '8005';
-const walletPort = '8460';
-const searchPort = '8461';
-const wishlistPort = '8412';
+
+// const authPort = '8005';
+// const walletPort = '8460';
+// const searchPort = '8461';
+// const wishlistPort = '8412';
 const porfolioPort = '8081'
+//
+// const gatewayPort = "8765";
+// const gateway =`${url}:${gatewayPort}`
+//
+// module.exports = {
+//    // authUrl: "http://localhost:8461/authenticateV2",
+//   authUrl: `${url}:${authPort}/authenticateV2`,
+//   registerURL: `${url}:${authPort}/registerV2`,
+//   validateUrl: `${url}:${authPort}/validateTokenV2`,
+//   validateOTP: `${url}:${authPort}/validateOTP`,
+//   checkOTPvalidation: `${url}:${authPort}/isOTPVerified?email=`,
+//   resendOTP: `${url}:${authPort}/sendOTP?email=`,
+//   // deleteUser: `${url}:${authPort}/email?email=`,
+//
+//   createWalletURL: `${url}:${walletPort}/wallet/create`,
+//   balanceURL: `${url}:${walletPort}/wallet/balance`,
+//   historiesURL: `${url}:${walletPort}/wallet/history`,
+//   transactURL: `${url}:${walletPort}/wallet/transact`,
+//   currencyURL: `${url}:${walletPort}/wallet/rates/`,
+//
+//   searchURL: `${url}:${searchPort}/mf/get/all`,
+//   getDetailsURL: `${url}:${searchPort}/mf/get/details`,
+//   getTopFiveURL: `${url}:${searchPort}/mf/get/top5`,
+//   getBottomFiveURL: `${url}:${searchPort}/mf/get/bottom5`,
+//
+//   addToWishlistURL: `${url}:${wishlistPort}/wishlist/add`,
+//   removeFromWishlistURL: `${url}:${wishlistPort}/wishlist/remove?wishlistId=`,
+//   wishlistExistURL: `${url}:${wishlistPort}/wishlist/exists?customerId=`,
+//   wishlistCustomerURL: `${url}:${wishlistPort}/wishlist/getbycustomerid?customerId=`,
+//   wishListSchemaURL: `${url}:${wishlistPort}/wishlist/exists?mfSchemaId=`,
+//
+//   portfoliosURL: `${url}:${porfolioPort}/portfolios/customers/`,
+//   buyMFURL: `${url}:${porfolioPort}/muf/purchase_muf`,
+//   withdrawMFURL: `${url}:${porfolioPort}/muf/withdraw_muf`,
+//   mfHistoryURL: `${url}:${porfolioPort}/muf/getCustomerMFHistory`,
+// };
+
+// const url = "http://localhost";
+const url = "http://35.183.238.7";
+
+const gatewayPort = "8765";
+const gateway =`${url}:${gatewayPort}`
+const mfservice = 'mutualfund'
 
 module.exports = {
-   // authUrl: "http://localhost:8461/authenticateV2",
-  authUrl: `${url}:${authPort}/authenticateV2`,
-  registerURL: `${url}:${authPort}/registerV2`,
-  validateUrl: `${url}:${authPort}/validateTokenV2`,
-  validateOTP: `${url}:${authPort}/validateOTP`,
-  checkOTPvalidation: `${url}:${authPort}/isOTPVerified?email=`,
-  resendOTP: `${url}:${authPort}/sendOTP?email=`,
+  // authUrl: "http://localhost:8461/authenticateV2",
+  authUrl: `${gateway}/auth/authenticateV2`,
+  registerURL: `${gateway}/auth/registerV2`,
+  validateUrl: `${gateway}/auth/validateTokenV2`,
+  validateOTP: `${gateway}/auth/validateOTP`,
+  checkOTPvalidation: `${gateway}/auth/isOTPVerified?email=`,
+  resendOTP: `${gateway}/auth/sendOTP?email=`,
+  deleteUser: `${gateway}/auth/email?email=`,
 
-  createWalletURL: `${url}:${walletPort}/wallet/create`,
-  balanceURL: `${url}:${walletPort}/wallet/balance`,
-  historiesURL: `${url}:${walletPort}/wallet/history`,
-  transactURL: `${url}:${walletPort}/wallet/transact`,
-  currencyURL: `${url}:${walletPort}/wallet/rates/`,
+  createWalletURL: `${gateway}/wallet/wallet/create`,
+  balanceURL: `${gateway}/wallet/wallet/balance`,
+  historiesURL: `${gateway}/wallet/wallet/history`,
+  transactURL: `${gateway}/wallet/wallet/transact`,
+  currencyURL: `${gateway}/wallet/wallet/rates/`,
 
-  searchURL: `${url}:${searchPort}/mf/get/all`,
-  getDetailsURL: `${url}:${searchPort}/mf/get/details`,
-  getTopFiveURL: `${url}:${searchPort}/mf/get/top5`,
-  getBottomFiveURL: `${url}:${searchPort}/mf/get/bottom5`,
+  searchURL: `${gateway}/mutualfund/mf/get/all`,
+  getDetailsURL: `${gateway}/mutualfund/mf/get/details`,
+  // getTopFiveURL: `${url}:${searchPort}/mf/get/top5`,
+  getTopFiveURL: `${gateway}/mutualfund/mf/get/top5`,
+  getBottomFiveURL: `${gateway}/mutualfund/mf/get/bottom5`,
+  // getBottomFiveURL: `http://localhost:8765/mutualfund/mf/get/top5`,
 
-  addToWishlistURL: `${url}:${wishlistPort}/wishlist/add`,
-  removeFromWishlistURL: `${url}:${wishlistPort}/wishlist/remove?wishlistId=`,
-  wishlistExistURL: `${url}:${wishlistPort}/wishlist/exists?customerId=`,
-  wishlistCustomerURL: `${url}:${wishlistPort}/wishlist/getbycustomerid?customerId=`,
-  wishListSchemaURL: `${url}:${wishlistPort}/wishlist/exists?mfSchemaId=`,
+  addToWishlistURL: `${gateway}/wishlist/wishlist/add`,
+  removeFromWishlistURL: `${gateway}/wishlist/wishlist/remove?wishlistId=`,
+  wishlistExistURL: `${gateway}/wishlist/wishlist/exists?customerId=`,
+  wishlistCustomerURL: `${gateway}/wishlist/wishlist/getbycustomerid?customerId=`,
+  wishListSchemaURL: `${gateway}/wishlist/wishlist/exists?mfSchemaId=`,
 
-  portfoliosURL: `${url}:${porfolioPort}/portfolios/customers/`,
-  buyMFURL: `${url}:${porfolioPort}/muf/purchase_muf`,
-  withdrawMFURL: `${url}:${porfolioPort}/muf/withdraw_muf`,
-  mfHistoryURL: `${url}:${porfolioPort}/muf/getCustomerMFHistory`,
+  portfoliosURL: `${gateway}/portfolio/portfolios/customers/`,
+  buyMFURL: `${gateway}/portfolio/muf/purchase_muf`,
+  withdrawMFURL: `${gateway}/portfolio/muf/withdraw_muf`,
+  mfHistoryURL: `${gateway}/portfolio/muf/getCustomerMFHistory`,
 };
